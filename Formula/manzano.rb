9781963ae048b8cd9b9ee7453fe6d34f95fa5f2b
Matt Ho <matt.ho@gmail.com>
2026-03-11 15:21:26 -0500

    manzano v0.1.3

--- Formula/manzano.rb
+++ Formula/manzano.rb
@@ -1,16 +1,16 @@
 class Manzano < Formula
   desc "Manzano development server"
   homepage "https://github.com/manzano-studio"
-  version "0.1.0"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.1.0/manzano-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "8c7c0ccfe6abdf2281d925a213b929ff1a3641ba6622a0e3b103bc7cf353aca5"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.1.3/manzano-0.1.3-aarch64-apple-darwin.tar.gz"
+      sha256 "1ba5b51c98a103fde0f7955c15af967482ba9819d5e495fe9549cb00b69b8683"
     else
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.1.0/manzano-0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "c32426161bad9eddb7b67f1bb74491ca1cd2be9892f585152c7dd05d272fa952"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.1.3/manzano-0.1.3-x86_64-apple-darwin.tar.gz"
+      sha256 "13d90a53622036f285ec8e1f9e9d4200021f058858be9412e7cb296968f6b84a"
     end
   end
 
