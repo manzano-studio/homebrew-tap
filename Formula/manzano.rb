33d92138a26e5c1eef4846d6117d0e1db141194f
Matt Ho <matt.ho@gmail.com>
2026-03-18 16:57:01 -0500

    manzano v0.2.3

--- Formula/manzano.rb
+++ Formula/manzano.rb
@@ -1,26 +1,26 @@
 class Manzano < Formula
   desc "Manzano development server"
   homepage "https://github.com/manzano-studio"
-  version "0.2.2"
+  version "0.2.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.2.2/manzano-0.2.2-aarch64-apple-darwin.tar.gz"
-      sha256 "cabb1c26e05ad89485b46a0aacb2d8ad21929c036aa9d9b686d95c8e8914b984"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.2.3/manzano-0.2.3-aarch64-apple-darwin.tar.gz"
+      sha256 "5d8c09570c5ccb3a4c8ea17ff42e177243915223f2369eba14606aea1659df5d"
     else
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.2.2/manzano-0.2.2-x86_64-apple-darwin.tar.gz"
-      sha256 "dc998f28f38860dda628020ec53f5e2ae5d90c817bfda07b151748da7d7385c2"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.2.3/manzano-0.2.3-x86_64-apple-darwin.tar.gz"
+      sha256 "1aed1dd0323318b7a3e81ac0f18e387ee2f9f4998f44133852600a5afb41c12f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.2.2/manzano-0.2.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6eb6c7c011440835e52c8adf01cfe71907d10c818c59089ebbf7eb9f53c82c6c"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.2.3/manzano-0.2.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f1e6eb33024900f5a61da0fe49c4dc49d0003ee5d226fa42834a75f23e33ff1b"
     else
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.2.2/manzano-0.2.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9624201b45c60350037a6f541d252c5e46d118c852305a040dd32394515a3416"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/v0.2.3/manzano-0.2.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f5058a07950aae15d89ff24d549ff564465b157a6004b9d1ace407b37fc38487"
     end
   end
 
