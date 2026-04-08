8f08c2783f53691e1edfa6b6b1695202b7580248
Matt Ho <matt.ho@gmail.com>
2026-04-08 18:15:08 -0500

    manzano v0.3.2

--- Formula/manzano.rb
+++ Formula/manzano.rb
@@ -1,26 +1,26 @@
 class Manzano < Formula
   desc "Manzano development server"
   homepage "https://github.com/manzano-studio"
-  version "0.3.1"
-  license "MIT"
+  version "0.3.2"
+  license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/manzano-v0.3.1/manzano-0.3.1-aarch64-apple-darwin.tar.gz"
-      sha256 "5201e6023bb63a867dee49adf7ca6efb76935fc50a3ab28909ee85da428ed3e1"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/manzano-v0.3.2/manzano-0.3.2-aarch64-apple-darwin.tar.gz"
+      sha256 "3eb502d3260372050bbb824624490128bc4ac7cf2f9f2f31aa7129e1e06ad700"
     else
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/manzano-v0.3.1/manzano-0.3.1-x86_64-apple-darwin.tar.gz"
-      sha256 "5bf03e137a5a070c2fd0b521363af02ebc8c0fe50bd942e9dca8a7266164c04c"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/manzano-v0.3.2/manzano-0.3.2-x86_64-apple-darwin.tar.gz"
+      sha256 "7d5dd3f8c08656cd14e8fec179eaabeb6b0a0679a17a75c8db5ee46689058d65"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/manzano-v0.3.1/manzano-0.3.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "20bca4dc5b9a3475e729970cdded2d9c19056834e238ba27820a4055b1177507"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/manzano-v0.3.2/manzano-0.3.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "66044f4f69b3683b71257ebc1ed9f8be424ab4c16d806b392000899e33d99f3b"
     else
-      url "https://github.com/manzano-studio/homebrew-tap/releases/download/manzano-v0.3.1/manzano-0.3.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "147a1ac3cbdc2773206a7696d8d841378c20ec755f3c6b3830b680672b23a0bf"
+      url "https://github.com/manzano-studio/homebrew-tap/releases/download/manzano-v0.3.2/manzano-0.3.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "c3a1bb5e1491316a1f9d9997f90d0699c905b0d801abbf9668faa3a4f2dd1316"
     end
   end
 
